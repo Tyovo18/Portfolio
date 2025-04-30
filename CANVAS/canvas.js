fe202ad5034a88cce7efa76d0254def9d114c56c
Tyovo18 <153129017+Tyovo18@users.noreply.github.com>
2025-04-30 11:54:36 +0200

    modifications pt 1 (retire les trucs inutiles)

--- CANVAS/canvas.js
+++ CANVAS/canvas.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener("DOMContentLoaded",init,false);
 
 function init(){
@@ -50,57 +49,4 @@ function init(){
         }
     gc.closePath();
    
-=======
-window.addEventListener("DOMContentLoaded",init,false);
-
-function init(){
-    var zone = document.getElementById("zoneDessin");
-    var gc = zone.getContext("2d");
-    
-    var cases = 0;
-    var horiz = 0;
-    var vert = 50;
-    gc.beginPath();
-    var couleur;
-    var ligne = 0;
-    var colonne = 0 ;
-    
-    while (cases<100){
-        
-       
-        if ((ligne+colonne)%2 == 1 ){
-            couleur="white";
-        }else{
-            couleur="black";
-        }
-        
-        if (colonne > 8 ){
-            
-            gc.fillStyle = couleur;
-            gc.strokeStyle = "black";
-            gc.fillRect(horiz,vert,25,25);
-            gc.strokeRect(horiz,vert,25,25);
-            
-            cases++;
-            horiz=0;
-            vert=vert+25;
-            ligne++;
-            colonne=0;
-        } else {
-            
-            gc.fillStyle = couleur;
-            gc.strokeStyle = "black";
-            gc.fillRect(horiz,vert,25,25);
-            gc.strokeRect(horiz,vert,25,25);
-            
-            cases++;
-            horiz=horiz+25;
-            colonne++;
-        }
-        
-
-        }
-    gc.closePath();
-   
->>>>>>> 50a9e963424efd6c3aea9bf41fc7f11ab236a8b0
 }
